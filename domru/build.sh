4984ca603624c45dede1eb8c4742bad92aa84eba
chOng-m0rrino <morrino.chong@gmail.com>
2026-06-13 14:54:26 +0300

    Update build.sh

--- domru/build.sh
+++ domru/build.sh
@@ -5,7 +5,7 @@ VERSION=`cat config.json | jq -r '.version'`
 
 echo "Running build for $VERSION"
 
-docker buildx build --progress=plain --pull --push -t wlad1164/domofon-amd64:latest   -t wlad1164/domofon-amd64:$VERSION   -f Dockerfile.amd64   .
-docker buildx build --progress=plain --pull --push -t wlad1164/domofon-armv7:latest   -t wlad1164/domofon-armv7:$VERSION   -f Dockerfile.armv7   .
-docker buildx build --progress=plain --pull --push -t wlad1164/domofon-aarch64:latest -t wlad1164/domofon-aarch64:$VERSION -f Dockerfile.aarch64 .
-docker buildx build --progress=plain --pull --push -t wlad1164/domofon-i386:latest    -t wlad1164/domofon-i386:$VERSION    -f Dockerfile.i386    .
+docker buildx build --progress=plain --pull --push -t chOng-m0rrino/domofon-amd64:latest   -t chOng-m0rrino/domofon-amd64:$VERSION   -f Dockerfile.amd64   .
+docker buildx build --progress=plain --pull --push -t chOng-m0rrino/domofon-armv7:latest   -t chOng-m0rrino/domofon-armv7:$VERSION   -f Dockerfile.armv7   .
+docker buildx build --progress=plain --pull --push -t chOng-m0rrino/domofon-aarch64:latest -t chOng-m0rrino/domofon-aarch64:$VERSION -f Dockerfile.aarch64 .
+docker buildx build --progress=plain --pull --push -t chOng-m0rrino/domofon-i386:latest    -t chOng-m0rrino/domofon-i386:$VERSION    -f Dockerfile.i386    .
